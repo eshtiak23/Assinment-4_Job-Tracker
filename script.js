e57f62e143c941e5e5c98bd21c479fb6636e896e
MD. ESHTIAK AHMED <eshtiak4099@gmail.com>
2026-02-24 11:12:36 +0600

    Added Delete button Logic's

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let currentTab = "all";
+let currentTab="all";
 
 const jobs = Array.from(document.querySelectorAll(".job")).map((card,i)=>({
   id:i,
@@ -7,26 +7,19 @@ const jobs = Array.from(document.querySelectorAll(".job")).map((card,i)=>({
 }));
 
 function showTab(tab){
-  currentTab = tab;
-
-  document.querySelectorAll(".tabBtn").forEach(btn=>{
-    btn.classList.remove("bg-blue-600","text-white");
-    btn.classList.add("bg-gray-200");
-  });
-
+  currentTab=tab;
   updateView();
 }
 
 function setStatus(button,status){
 
-  const card = button.closest(".job");
-
-  const job = jobs.find(j=>j.el===card);
+  const card=button.closest(".job");
+  const job=jobs.find(j=>j.el===card);
   if(!job) return;
 
-  job.status = status;
+  job.status=status;
 
-  const badge = card.querySelector(".status");
+  const badge=card.querySelector(".status");
 
   if(status==="interview"){
     badge.textContent="INTERVIEW";
@@ -36,10 +29,24 @@ function setStatus(button,status){
     badge.className="status text-xs bg-red-100 text-red-700 inline-block px-2 py-1 mt-2";
   }
 
-  updateView(); 
+  updateView();
 }
 
-function  updateView(){
+function deleteJob(button){
+
+  const card=button.closest(".job");
+
+  const index=jobs.findIndex(j=>j.el===card);
+  if(index>-1){
+    jobs.splice(index,1);   // remove from array
+  }
+
+  card.remove();            // remove from screen
+
+  updateView();
+}
+
+function updateView(){
 
   let interview=0;
   let rejected=0;
@@ -54,38 +61,15 @@ function  updateView(){
       job.el.style.display="block";
       visible++;
     }else{
-      job.el.style.display="none";
+      job.el.style.display="none" ;
     }
 
-  }) ;
+  });
 
   document.getElementById("interviewCount").textContent=interview;
   document.getElementById("rejectedCount").textContent=rejected;
   document.getElementById("tabCount").textContent=visible+" jobs";
 
-  const empty=document.getElementById("emptyState");
-  const title=document.getElementById("emptyTitle");
-  const sub=document.getElementById("emptySub");
-
-  if(currentTab!=="all" && visible===0){
-
-    empty.classList.remove("hidden");
-
-    if(currentTab==="interview"){
-      title.textContent="No interview jobs yet";
-      sub.textContent="Jobs you mark as Interview will appear here";
-    }
-
-
-    if(currentTab==="rejected"){
-      title.textContent="No rejected jobs yet";
-      sub.textContent="Jobs you mark as Rejected will appear here";
-    }
-
-
-  }else{
-    empty.classList.add("hidden");
-  }
 }
 
 updateView();
